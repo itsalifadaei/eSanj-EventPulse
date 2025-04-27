1df76cf35565ed57d05a5fa41665a1a9ffdc9041
Ali Fadaei <alifadaei_123@yahoo.com>
2025-04-27 19:04:49 +0330

    Chore: mirror change

--- event-ingestor/bootstrap/app.php
+++ event-ingestor/bootstrap/app.php
@@ -5,7 +5,9 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting()
+    ->withRouting(
+        health: '/up',
+    )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
