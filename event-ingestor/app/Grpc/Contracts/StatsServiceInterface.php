6d5028d4b4ba1e54a02f06983672284e547b7e0b
Ali Fadaei <alifadaei_123@yahoo.com>
2025-04-27 21:02:43 +0330

    Chore: mirror change

--- event-ingestor/app/Grpc/Contracts/StatsServiceInterface.php
+++ event-ingestor/app/Grpc/Contracts/StatsServiceInterface.php
@@ -3,13 +3,13 @@
 namespace App\Grpc\Contracts;
 
 use Spiral\RoadRunner\GRPC\ContextInterface;
-use Telemetry\{DailyStatsRequest, DailyStatsResponse, TopUsersRequest, TopUsersResponse};
+use Telemetry\{HourlyStatsRequest, HourlyStatsResponse, TopUsersRequest, TopUsersResponse};
 
 interface StatsServiceInterface
 {
     const NAME = "telemetry.EventQuery";
 
-    public function GetDailyStats(ContextInterface $ctx, DailyStatsRequest $req): DailyStatsResponse;
+    public function GetHourlyStats(ContextInterface $ctx, HourlyStatsRequest $req): HourlyStatsResponse;
 
     public function GetTopUsers(ContextInterface $ctx, TopUsersRequest $req): TopUsersResponse;
 }
--- query-api/bootstrap/app.php
+++ query-api/bootstrap/app.php
@@ -6,7 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-
+        api: __DIR__ . '/../routes/api.php',
+        apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
